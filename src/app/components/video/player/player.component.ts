886fb78ae55f4beb0466ad21d6abbf810e98e180
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-12-15 09:33:00 +0100

    feat: added quality selection function

--- src/app/components/video/player/player.component.ts
+++ src/app/components/video/player/player.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { VgApiService, VgCoreModule } from '@videogular/ngx-videogular/core';
+import { BitrateOptions, VgApiService, VgCoreModule } from '@videogular/ngx-videogular/core';
 import { VgControlsModule } from '@videogular/ngx-videogular/controls';
 import { VgOverlayPlayModule } from '@videogular/ngx-videogular/overlay-play';
 import { VgBufferingModule } from '@videogular/ngx-videogular/buffering';
@@ -13,34 +13,14 @@ import { VideoMeta } from '../../../shared/models/video-meta';
   templateUrl: './player.component.html',
   styleUrl: './player.component.scss'
 })
-export class PlayerComponent implements OnInit {
+export class PlayerComponent {
   @Input({ required: true }) videoMeta!: VideoMeta;
   @ViewChild('media') videoRef!: ElementRef;
   api: VgApiService = new VgApiService;
-  readonly ALLOWED_RESOLUTIONS: number[] = [480, 720, 1080];
-  videoResolutionInP: number = this.ALLOWED_RESOLUTIONS[1];
-
-
-  ngOnInit(): void {
-    this.setResAuto();
-  }
+  hlsBitrates?: BitrateOptions[];
 
 
   onPlayerReady(source: VgApiService) {
     this.api = source;
-  }
-  
-
-  setResAuto(): void {
-    const maxAllowedRes: number = this.ALLOWED_RESOLUTIONS[this.ALLOWED_RESOLUTIONS.length - 1];
-    const minResLargerThanVh: number = Math.min(
-      ...this.ALLOWED_RESOLUTIONS.filter(r => r >= window.innerHeight)
-    );
-    this.videoResolutionInP = minResLargerThanVh > maxAllowedRes ? maxAllowedRes : minResLargerThanVh;
-  }
-
-
-  setRes(res: 480 | 720 | 1080 | 'auto'): void {
-    res == 'auto' ? this.setResAuto() : this.videoResolutionInP = res;
   }
 };
