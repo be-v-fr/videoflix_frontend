522d08134cadd986d8edf01f78dca036f5c3c58c
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2024-12-17 19:12:01 +0100

    feat: fancy nav init on user init

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, Type } from '@angular/core';
+import { Component, OnDestroy, OnInit, Type } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { LogoComponent } from '../../shared/components/logo/logo.component';
@@ -7,6 +7,7 @@ import { SearchComponent } from '../../shared/components/search/search.component
 import { DialogComponent } from '../dialog/dialog.component';
 import { RequestPwResetComponent } from '../auth/pw-reset/request-pw-reset/request-pw-reset.component';
 import { ToastNotificationComponent } from '../../shared/components/toast-notification/toast-notification.component';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navigation',
@@ -15,15 +16,37 @@ import { ToastNotificationComponent } from '../../shared/components/toast-notifi
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.scss'
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit, OnDestroy {
   requestPwResetComponent: Type<object> = RequestPwResetComponent;
   changePwDialogShowing: boolean = false;
   loggedOut: boolean = false;
+  awaitingInit: boolean | 'complete' = false;
+  authSub: Subscription = new Subscription();
 
   constructor(
     public router: Router,
     public authService: AuthService
   ) { }
+
+  ngOnInit(): void {
+    if(!this.authService.currentUser) {
+      this.awaitingInit = true;
+      this.authSub = this.subAuth();
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.authSub.unsubscribe();
+  }
+
+  subAuth(): Subscription {
+    return this.authService.currentUser$.subscribe(user => {
+      if(user) {
+        this.awaitingInit = 'complete';
+        this.authSub.unsubscribe();
+      }
+    });
+  }
 
   showChangePwDialog(): void {
     this.changePwDialogShowing = true;
