11b3680fd1d3dcacfd8a2c71cf22466d831a25b9
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-01-11 13:25:36 +0100

    feat: further dynamic video display optimization

--- src/app/components/home/videos-category/videos-category.component.ts
+++ src/app/components/home/videos-category/videos-category.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, ViewChild, ElementRef, HostListener, OnInit, AfterViewInit } from '@angular/core';
 import { VideoMeta } from '../../../shared/models/video-meta';
 import { VideoCompletion } from '../../../shared/models/video-completion';
 import { VideoCardComponent } from '../video-card/video-card.component';
@@ -12,13 +12,17 @@ import { StyleService } from '../../../shared/services/style.service';
   templateUrl: './videos-category.component.html',
   styleUrl: './videos-category.component.scss'
 })
-export class VideosCategoryComponent {
+export class VideosCategoryComponent implements AfterViewInit {
   @Input({ required: true }) title!: string;
   @Input({ required: true }) selection!: VideoMeta[];
   @Input({ required: true }) categoryIndex!: number;
   @Output() continue: EventEmitter<{ meta: VideoMeta, completion: VideoCompletion }> = new EventEmitter();
   @Output() details: EventEmitter<{ meta: VideoMeta, completion?: VideoCompletion }> = new EventEmitter();
   expanded: boolean = false;
+  @ViewChild('container') containerRef!: ElementRef<HTMLDivElement>;
+  moreThanOneRow: boolean = true;
+  private lastWindowEventHandling = 0;
+  private onWindowEventThrottleInterval = 100;
 
 
   constructor(
@@ -26,7 +30,43 @@ export class VideosCategoryComponent {
   ) { }
 
 
+  ngAfterViewInit(): void {
+    this.checkRows();
+  }
+
+
   toggleExpansion(): void {
     this.expanded = !this.expanded;
+  }
+
+
+  @HostListener('window:resize')
+  onResize() {
+    const now = Date.now();
+    if (now - this.lastWindowEventHandling >= this.onWindowEventThrottleInterval) {
+      this.lastWindowEventHandling = now;
+      this.checkRows();
+    }
+  }
+
+
+  checkRows(): void {
+    const children = Array.from(this.containerRef.nativeElement.children) as HTMLElement[];
+    children.length === this.selection.length ? this.checkVerticalSize(children) : this.checkHorizontalSize(children);
+  }
+
+
+  checkVerticalSize(children: HTMLElement[]): void {
+    const containerHeight: number = this.containerRef.nativeElement.offsetHeight;
+    const videoHeight: number = children[0].offsetHeight;
+    this.moreThanOneRow = containerHeight > 2 * videoHeight;
+  }
+
+
+  checkHorizontalSize(children: HTMLElement[]): void {
+    const containerWidth: number = this.containerRef.nativeElement.offsetWidth;
+    const videoWidth: number = children[0].offsetWidth;
+    const videoGap: number = children[1].offsetLeft - children[0].offsetLeft - videoWidth;
+    this.moreThanOneRow = containerWidth + videoGap < this.selection.length * (videoWidth + videoGap);
   }
 }
